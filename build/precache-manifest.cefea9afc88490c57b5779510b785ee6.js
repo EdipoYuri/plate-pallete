77c6027afdee81d91574ae4d2a856e28d265d3d8
Édipo Yuri Ribeiro da Silva <edipoyurirs@gmail.com>
2020-12-17 23:52:45 -0300

    Building project

---
+++ build/precache-manifest.cefea9afc88490c57b5779510b785ee6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ade6d8e5586cce15b9265e72e7ce87c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b903654a386221437bb1",
+    "url": "/static/css/main.5ab88178.chunk.css"
+  },
+  {
+    "revision": "3a7de60b5387624a44b2",
+    "url": "/static/js/2.67ede4c1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.67ede4c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b903654a386221437bb1",
+    "url": "/static/js/main.6e0aae0e.chunk.js"
+  },
+  {
+    "revision": "f7239f31680a455aa1a0",
+    "url": "/static/js/runtime-main.478c4048.js"
+  },
+  {
+    "revision": "a2076e40a41c020a421106dbdd4a345a",
+    "url": "/static/media/background.a2076e40.jpg"
+  },
+  {
+    "revision": "ea9c8f8a92084bda34c18eedd0b305ba",
+    "url": "/static/media/bag.ea9c8f8a.jpg"
+  },
+  {
+    "revision": "9245a213a913921c7b023c925be51399",
+    "url": "/static/media/burger.9245a213.jpg"
+  },
+  {
+    "revision": "74a87e6b9ce9897b6376e3c3240d768b",
+    "url": "/static/media/plate.74a87e6b.jpg"
+  },
+  {
+    "revision": "726a4b2bb39c1bda96b9f53d5cc443b3",
+    "url": "/static/media/recipes-banner.726a4b2b.jpg"
+  }
+]);
